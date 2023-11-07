6d74029c601acd578065a62d9f2fa6da6b2127b0
Vitor Geovani <sincerev36@gmail.com>
2023-11-07 17:57:49 -0300

    Enviando os arquivos

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuButton.addEventListener("click", function () {
+        mobileMenu.style.transform = "translateX(0)";
+    });
+
+    mobileMenu.addEventListener("click", function () {
+        mobileMenu.style.transform = "translateX(100%)";
+    });
+});
